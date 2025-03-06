0baa0b2939ff5713876ab0951d110b2b506c225c
Amir Taheri <amir.taheri.web@protonmail.com>
2025-03-06 15:43:28 +0330

    fix: bugs fixed

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const productRoutes = require('./routes/productRoutes');
-const authRoutes = require('./routes/authRoutes');
-const swaggerUi = require('swagger-ui-express');
+const express = require("express");
+const cors = require("cors");
+const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
+const swaggerUi = require("swagger-ui-express");
+require('dotenv').config()
 
 // Load swagger configuration
-let swaggerDocument = require('./swagger/swagger.json');
+let swaggerDocument = require("./swagger/swagger.json");
 
 const app = express();
-let PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const BASE_URL = process.env.BASE_URL;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -17,37 +19,39 @@ app.use(express.json());
 app.use(cors());
 
 // Routes
-app.use('/book', productRoutes);
-app.use('/auth', authRoutes);
+app.use("/book", productRoutes);
+app.use("/auth", authRoutes);
 
 // Helper function to start server and try the next port if the current one is taken
-function startServer(port) {
+function startServer(port, baseUrl) {
   const server = app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+    console.log(`Server is running on ${baseUrl}:${port}`);
 
     // Update Swagger with the correct port
     swaggerDocument.servers = [
       {
-        url: `http://localhost:${port}`,
-        description: "Local server"
-      }
+        url: `${baseUrl}:${port}`,
+        description: "Local server",
+      },
     ];
 
     // Serve updated Swagger docs
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-    console.log(`Swagger API docs are available at http://localhost:${port}/api-docs`);
+    app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    console.log(
+      `Swagger API docs are available at ${baseUrl}:${port}/api-docs`
+    );
   });
 
   // Handle error in case the port is in use
-  server.on('error', (err) => {
-    if (err.code === 'EADDRINUSE') {
+  server.on("error", (err) => {
+    if (err.code === "EADDRINUSE") {
       console.log(`Port ${port} is in use, trying port ${port + 1}...`);
       startServer(port + 1); // Try the next port
     } else {
-      console.error('Server error:', err);
+      console.error("Server error:", err);
     }
   });
 }
 
 // Start the server and handle port availability
-startServer(PORT);
+startServer(PORT, BASE_URL);
